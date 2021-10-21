16097a7ae733054932e97c5ec2e1c78f43a314d8
Danka02 <kamikoto02@mail.ru>
2021-10-21 18:22:33 +0300

    Initial commit

---
+++ src/main/kotlin/PostData.kt
@@ -0,0 +1,5 @@
+object PostData {
+    fun getPostBase() = arrayListOf<Post>( Post(1,
+        "lg",234,"lal alal", like = false, comment = false, share = false, 1, 2, 3, "Деменьтева 12", Pair(23.434, 23.4324),type = Types.YoutubeVideo))
+
+}
