9cb2fbd5db3a020670f314d3104d8dc03ade7d49
Danka02 <kamikoto02@mail.ru>
2021-10-22 20:16:16 +0300

    ver:2

--- src/main/kotlin/PostData.kt
+++ src/main/kotlin/PostData.kt
@@ -1,28 +1,25 @@
 object PostData {
-    fun getPostBase() = arrayListOf<Post>( Post(1,
-        "lg",234,"lal alal",
-        like = false, comment = false,
-        share = false, 0,
-        0, 0,
-        "Деменьтева 12",
-        Pair(23.434, 23.4324),
-        type = Types.YoutubeVideo),
+    fun getPostBase() = arrayListOf<Post>( Post(1,"Дан",1634582976283,"https://www.youtube.com/watch?v=4haurn3S8z8",
+        like = false,
+        comment = false,
+        share = false,0,0,0,"Дементьева 12",
+        coordinates = Pair(55.84058,38.20251),type = Types.YoutubeVideo,hidePost = false),
 
-        Post (2, "Dan",234,"hello",
-            like = false, comment = false,
-            share = false, 0,
-            0, 0,
-            "Деменьтева 12",
-            Pair(23.434, 23.4324),
-            type = Types.Reposts),
+        Post (2,"Ил",1634583544357,"https://www.youtube.com/watch?v=vd205Q_EE18",
+            like = false,
+            comment = false,
+            share = false,
+            0,0,0,
+            "Дементьева 12",coordinates = Pair(55.84058,38.20251),
+            type = Types.YoutubeVideo,hidePost = false),
 
-        Post (3, "Il",234,"world",
-            like = false, comment = false,
-            share = false, 0,
-            0, 0,
-            "Деменьтева 12",
-            Pair(23.434, 23.4324),
-            type = Types.SponsoredPosts))
+        Post (3,"Хайам",1634650874450,"https://sun1-30.userapi.com/impg/1MiP49SbCxyk8KHQ4svLR10yGIHbUO2ZDdaVCg/Ug4DJFiZ1r0.jpg?size=1280x853&quality=96&sign=a48427054c53e50b6f7deb128d8ccc40&type=album",
+            like = false,
+            comment = false,
+            share = false,
+            0,0,0,
+            "Дементьева 12",coordinates = Pair(55.84058,38.20251),
+            type = Types.SponsoredPosts,hidePost = false,url = "https://vk.com/marvel_dc"))
 
 
 }
