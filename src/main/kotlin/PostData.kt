44ddd3cfb9f2aef3b81d99f4326ca3cd7a11ebfd
Danka02 <kamikoto02@mail.ru>
2021-10-23 16:23:05 +0300

    ver:3

--- src/main/kotlin/PostData.kt
+++ src/main/kotlin/PostData.kt
@@ -21,5 +21,20 @@ object PostData {
             "Дементьева 12",coordinates = Pair(55.84058,38.20251),
             type = Types.SponsoredPosts,hidePost = false,url = "https://vk.com/marvel_dc"))
 
-
+    fun getPostAdvertising() =  arrayListOf<Post>(Post(7,"Пятница",1634650874450,"https://sun1-99.userapi.com/impf/c844722/v844722724/1d8dcf/BkPdJtPQNn8.jpg?size=809x1080&quality=96&sign=184affb24657e9265b9bf5de75905751&type=album",
+    like = false,
+    comment = false,
+    share = false,
+    0,0,0,
+        "Дементьева 12",coordinates = Pair(55.84058,38.20251),
+        type = Types.SponsoredPosts,hidePost = false,url = "https://vk.com/marvel_dc"
+    ),
+    Post(8,"Дойл",1634650874450,"https://sun1-22.userapi.com/impf/c622827/v622827349/44cfb/RtkiymBF8PY.jpg?size=900x900&quality=96&sign=cb8cfff238701b08cccf56d79382f046&type=album",
+    like = false,
+    comment = false,
+    share = false,
+    0,0,0,"Дементьева 12",coordinates = Pair(55.84058,38.20251),
+        type = Types.SponsoredPosts,hidePost = false,url = "https://vk.com/marvel_dc"))
 }
+
+
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -6,4 +6,9 @@ val post = "post.json"
     val posts = PostData.getPostBase()
     val json = Gson().toJson(posts)
     File(post).writeText(json)
+
+    val post1 = "advertising.json"
+    val posts1 = PostData.getPostAdvertising()
+    val json1 = Gson().toJson(posts1)
+    File(post1).writeText(json1)
 }
